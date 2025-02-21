c2ffdefbe5ed72b342d4fb02167d8785714fbdf2
Carlos Rosario <crosari5@my.centennialcollege.ca>
2025-02-21 16:09:36 -0500

    updating welcome message

--- src/main/java/com/centennial/devops/lab2question2/HomeController.java
+++ src/main/java/com/centennial/devops/lab2question2/HomeController.java
@@ -1,13 +1,20 @@
 package com.centennial.devops.lab2question2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalTime;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String showWelcomePage(){
+    public String showWelcomePage(Model model){
+        String name = "Carlos Rosario"; // Replace with your name
+        LocalTime now = LocalTime.now();
+        String greeting = (now.getHour() < 12) ? "Good morning" : "Good afternoon";
+        model.addAttribute("greeting",greeting + " " + name + ", Welcome to COMP367");
         return "welcome";
     }
 }
